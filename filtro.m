1999d477574eaded3e189b147bf3820b41206967
Manuel Cabral <mcabral@frba.utn.edu.ar>
2022-10-11 18:12:04 -0300

    feat: noise + grayscale filter

--- filtro.m
+++ filtro.m
@@ -1,10 +1,21 @@
+# require package
+pkg load image
 tic();
 
+# loadimage
 image = imread("lenna.png");
-grayval = 0.4*image(:,:,1) + 0.2*image(:,:,2) + 1.2*image(:,:,3);
-mask = (grayval > 152);
-result = image.* repmat(mask, [1 1 1]);
-imwrite(result, "lenna2.png");
+
+# convert to gray scale
+gs_image = rgb2gray(image);
+
+# apply noise to the image
+noise_image = imnoise(gs_image, "gaussian");
+
+# shows image
+imshow(noise_image);
+
+# descomment for save the image
+# imwrite(noise_image, "lenna_noise.png");
 
 elapsed_time = toc();
 disp(elapsed_time)
