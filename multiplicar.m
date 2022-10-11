20f47d8a86f34b8dac3819f78c3f881c1fb88cd3
Manuel Cabral <mcabral@frba.utn.edu.ar>
2022-10-11 18:34:13 -0300

    fix: disp

--- multiplicar.m
+++ multiplicar.m
@@ -4,9 +4,13 @@ tic();
 # load csv file
 data = csvread("abalone.data.csv");
 
+# generate second matrix data from data
 second_data = data*2;
 
+# multiply together
 multiplied_data = data .* second_data;
+
+disp(multiplied_data);
 
 elapsed_time = toc();
 disp(elapsed_time)
